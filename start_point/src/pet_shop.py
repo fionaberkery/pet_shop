f916f84bc6b5269045cd4a10f5b9d202df191ab4
Fiona <fiona_berkery@hotmail.com>
2022-04-03 10:25:45 +0100

    q3 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -5,4 +5,9 @@ def get_pet_shop_name(shop):
     
 def get_total_cash(shop):
     return shop["admin"]["total_cash"]
-    
+
+def add_or_remove_cash(shop, amount):
+    shop["admin"]["total_cash"] += amount
+
+
+
